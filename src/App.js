89fbdce27d30341db2e1180fba3859240a1e390d
tanviAggarwal001 <tanviaggarwal0097@gmail.com>
2025-03-21 01:01:31 +0530

    first pages - signup and login frontend

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App; 
